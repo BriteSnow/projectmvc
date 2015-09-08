ca2d357f927bf2f249aa5aa8e41397dc12576b80
jeremychone <jeremy.chone@gmail.com>
2015-09-08 14:18:03 -0700

    . updated package json with hbsp/vdev dependency

--- src/main/webapp/common/js/4_render.js
+++ src/main/webapp/common/js/4_render.js
@@ -2,11 +2,14 @@
 // Just a little indirection to render a template using handlebars.
 // This simple indirection allows much flexibility later one, 
 // when using pre-compiling or other templating engine are needed.
+
+// The node.js hbsp process will put the precompile function in the Handlebars.templates
 Handlebars.templates = Handlebars.templates || {};
 
 $(function(){
-	// Make all templates partials (no reasons why they should not)
-	// Note: We put this in a jQuery.ready to make sure the Handlebars.templates where loaded. 
+	// Make all templates partials (no reason why they should not)
+	// Note: We put this in a jQuery.ready to make sure the Handlebars.templates where loaded (as they should be loaded
+	//	     in the head). 
 	//       This assumes the "templates.js" is loaded in the <head></head> (which is the case in our best practice)
 	Handlebars.partials =  Handlebars.templates;	
 });
@@ -15,6 +18,7 @@ $(function(){
 function render(templateName,data){
 	var tmpl = Handlebars.templates[templateName];
 
+	// if not found, try to get it from the DOM and assume the full handlebars
 	if (!tmpl){
 		var html = $("#" + templateName).html();
 		if (!html){
@@ -23,6 +27,8 @@ function render(templateName,data){
 		tmpl = Handlebars.compile(html);
 		Handlebars.templates[templateName] = tmpl;
 	}
+
+	// call the function and return the result
 	return tmpl(data);
 }
 // --------- /Render --------- //
