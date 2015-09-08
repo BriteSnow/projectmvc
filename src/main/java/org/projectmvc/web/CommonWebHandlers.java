60dc627da275fff59a557e4790f976dda5fae4fb
jeremychone <jeremy.chone@gmail.com>
2015-09-08 15:58:51 -0700

    . appVersion to DROP-001

---
+++ src/main/java/org/projectmvc/web/CommonWebHandlers.java
@@ -0,0 +1,17 @@
+package org.projectmvc.web;
+
+import com.britesnow.snow.web.handler.annotation.WebModelHandler;
+import com.britesnow.snow.web.param.annotation.WebModel;
+import com.google.inject.Singleton;
+import org.projectmvc.AppConfig;
+
+import java.util.Map;
+
+@Singleton
+public class CommonWebHandlers {
+
+	@WebModelHandler(startsWith="/")
+	public void allWebPages(@WebModel Map model){
+		model.put("appVersion", AppConfig.appVersion);
+	}
+}
