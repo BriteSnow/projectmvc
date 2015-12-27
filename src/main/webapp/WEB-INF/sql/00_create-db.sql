742d45a34366a345d9d6df752224ec47dd43443a
jeremychone <jeremy.chone@gmail.com>
2015-12-27 08:48:08 -0800

    . update 00 create sql to make role case insensitive

--- src/main/webapp/WEB-INF/sql/00_create-db.sql
+++ src/main/webapp/WEB-INF/sql/00_create-db.sql
@@ -1,4 +1,4 @@
 DROP DATABASE IF EXISTS "projectmvc_db";
 DROP USER IF EXISTS "projectmvc_user";
 CREATE USER "projectmvc_user" PASSWORD 'welcome';
-CREATE DATABASE "projectmvc_db" owner projectmvc_user ENCODING = 'UTF-8';
+CREATE DATABASE "projectmvc_db" owner "projectmvc_user" ENCODING = 'UTF-8';
